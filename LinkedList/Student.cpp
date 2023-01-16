0dcd2ce91617b1661c46500b14e1bb3226a789df
Colby Waters <colbywaters04@gmail.com>
2023-01-15 19:35:00 -0800

    [LinkedList] Updated comments in Student.cpp

--- LinkedList/Student.cpp
+++ LinkedList/Student.cpp
@@ -13,29 +13,44 @@
 
 #include "Student.h"
 
-    Student::Student()
-    { 
-       mId = 0;
-       strcpy(mName, "");
-    }
+/*
+ * Default constructor for Student
+ */
+Student::Student()
+{ 
+   mId = 0;
+   strcpy(mName, "");
+}
 
-    void Student::setId(int id)
-    {
-       mId = id;
-    }
+/*
+ * Method to set student id.
+ */
+void Student::setId(int id)
+{
+   mId = id;
+}
 
-    void Student::setName(const char* name)
-    {
-       strcpy(mName, name);
-    }
+/*
+ * Method to set student name.
+ */
+void Student::setName(const char* name)
+{
+   strcpy(mName, name);
+}
 
-    const char* Student::getName()
-    {
-        return mName;
-    }
+/*
+ * Method to get student name.
+ */
+const char* Student::getName()
+{
+   return mName;
+}
 
-    int Student::getId()
-    {
-        return mId;
-    }
+/*
+ * Method to get student id.
+ */
+int Student::getId()
+{
+   return mId;
+}
 
