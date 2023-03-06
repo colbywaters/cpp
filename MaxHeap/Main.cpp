0ad69f20d40a1aeda07da6839d3fdbc8e3b893f6
Colby Waters <colbywaters04@gmail.com>
2023-03-05 23:26:45 -0800

    Add main function to call MaxHeap.

---
+++ MaxHeap/Main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+
+#include "MaxHeap.h"
+
+using namespace std;
+
+/*
+ * Main implementation for using Max Heap data structure.
+ */
+int main() {
+    MaxHeap heap;
+
+    cout << endl;
+    cout << "--------------------------------------------------" << endl;
+    cout << "1. Enter a series of numbers." << endl;
+    cout << "2. Enter filename containing series of numbers." << endl;
+    cout << "--------------------------------------------------" << endl;
+
+    cout << "Make your choice (1 or 2): ";
+    int choice;
+    cin >> choice;
+
+    if (choice == 1) {
+        cout << "Enter up to 100 integers (separated by spaces). Enter -1 to stop: ";
+        int value;
+        while (cin >> value && value != -1)
+            heap.insert(value);
+    } else if (choice == 2) {
+        /* Todo! */
+    } else {
+        cout << "Invalid choice! It needs to be either 1 or 2." << endl;
+        return 0;
+    }
+
+    return 0;
+}
